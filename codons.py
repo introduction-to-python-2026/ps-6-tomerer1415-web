2c20c3c24537ea105e62f2e5ba3edf0a082d5c11
Tomer erez <tomerer1415@gmail.com>
2025-12-02 12:46:32 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,18 +1,15 @@
 def create_codon_dict(file_path):
- path = "/content/data/codons.txt"
- file = open(path)
- rows = file.readlines()
- file.close()
- codon_dict = {}
- for row in rows:
+    path = "/content/data/codons.txt"
+    file = open(path)
+    rows = file.readlines()
+    file.close()
+    
+    codon_dict = {}
+    for row in rows:
       parts = row.strip().split('\t')
-
-      if len(parts) < 3:
-          continue
-
       codon = parts[0]
       amino_acid = parts[2]
 
       codon_dict[codon] = amino_acid
 
- return codon_dict
+     return codon_dict
